c6ba40246f65a921d30cae713f0ee01d6e93689c
Amman Vedi <amman.vedi@gmail.com>
2013-10-24 00:38:15 +0100

    test change

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <Title>Registration Form</Title>
 </head>
 <body>
-<h1>Register here!</h1>
+<h1>Register here!!!!!!!</h1>
 <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
 <form method="post" action="index.php" enctype="multipart/form-data" >
       Name  <input type="text" name="name" id="name"/></br>
